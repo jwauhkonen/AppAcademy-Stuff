3b1667b31d3fcc574d48fe67e6aa435ff7c8c3c2
Jonathan Wauhkonen <jwauhkonen@gmail.com>
2014-11-18 22:05:39 -0800

    Pokedex day 2

--- W7D1/skeleton/app/assets/javascripts/pokedex-1B.js
+++ W7D1/skeleton/app/assets/javascripts/pokedex-1B.js
@@ -1,4 +1,11 @@
 Pokedex.RootView.prototype.renderPokemonDetail = function (pokemon) {
+	var divString = "<div class='detail' img src='" + pokemon.attributes.image_url + "'>";
+	
+	
+	
+	var divString += "</div>";
+	
+	this.$pokeDetail.append(divString); 
 };
 
 Pokedex.RootView.prototype.selectPokemonFromList = function (event) {
